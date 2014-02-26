3294bd0988f39b4dd35725ee8e24c2f5cdc83376
raphael.pillie@gmail.com <raphael.pillie@gmail.com@f0f721dd-ee35-590d-ed42-93f5ef06c5ec>
2014-02-26 13:05:34 +0000

    Ajout des classes d'affichage

---
+++ OptimisationCombinatoire/src/modele/Ecran_console.java
@@ -0,0 +1,15 @@
+package modele;
+
+public class Ecran_console extends Affichage
+{
+	protected void afficher_message(Object message)
+	{
+		System.out.println(message);
+	}
+
+	@Override
+	protected void reset_message()
+	{
+
+	}
+}
