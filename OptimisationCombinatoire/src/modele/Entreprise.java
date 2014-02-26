4a8bbf5caf72ed453691e369ace74bf43a31a82b
raphael.pillie@gmail.com <raphael.pillie@gmail.com@f0f721dd-ee35-590d-ed42-93f5ef06c5ec>
2014-02-26 14:02:29 +0000

    Implémentation des méthode de Base et Entreprise

--- OptimisationCombinatoire/src/modele/Entreprise.java
+++ OptimisationCombinatoire/src/modele/Entreprise.java
@@ -12,5 +12,38 @@ public class Entreprise
 	/**
 	 * Liste des bases dans lesquelle l'entreprise est présente
 	 */
-	private ArrayList<Base> Bases;
+	private ArrayList<Base> bases;
+
+	public Entreprise(String nom, ArrayList<Base> bases)
+	{
+		this.nom = nom;
+		this.bases = bases;
+	}
+
+	public Entreprise(String nom)
+	{
+		this.nom = nom;
+		this.bases = new ArrayList<Base>();
+	}
+
+	public String getNom()
+	{
+		return nom;
+	}
+
+	public void setNom(String nom)
+	{
+		this.nom = nom;
+	}
+
+	public ArrayList<Base> getBases()
+	{
+		return bases;
+	}
+
+	public void setBases(ArrayList<Base> bases)
+	{
+		this.bases = bases;
+	}
+
 }
