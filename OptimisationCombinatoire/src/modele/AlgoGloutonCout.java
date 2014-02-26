7a8624156add20340c0bc4469ec3640ed2789d2b
raphael.pillie@gmail.com <raphael.pillie@gmail.com@f0f721dd-ee35-590d-ed42-93f5ef06c5ec>
2014-02-26 16:39:55 +0000

    ajout du fichier -_-

---
+++ OptimisationCombinatoire/src/modele/AlgoGloutonCout.java
@@ -0,0 +1,13 @@
+package modele;
+
+public class AlgoGloutonCout extends AlgoRecherche
+{
+
+	@Override
+	public void rechercher(Scenario scenario)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
