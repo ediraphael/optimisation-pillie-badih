1f9b168471b7a9a84d8a656b8eb72c7aa7e179f6
raphael.pillie@gmail.com <raphael.pillie@gmail.com@f0f721dd-ee35-590d-ed42-93f5ef06c5ec>
2014-02-26 13:38:18 +0000

    Modification des import de scnéario Ajout d'une class Base

--- OptimisationCombinatoire/src/modele/ParserScenario.java
+++ OptimisationCombinatoire/src/modele/ParserScenario.java
@@ -3,7 +3,6 @@ package modele;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public abstract class ParserScenario extends ParserFichier
 {
