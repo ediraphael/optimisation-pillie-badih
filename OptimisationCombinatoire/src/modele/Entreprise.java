d6edfc4522a643d38652640a8c083c42e02c1c93
raphael.pillie@gmail.com <raphael.pillie@gmail.com@f0f721dd-ee35-590d-ed42-93f5ef06c5ec>
2014-02-26 13:59:41 +0000

    Implémentation de la class Entreprise

--- OptimisationCombinatoire/src/modele/Entreprise.java
+++ OptimisationCombinatoire/src/modele/Entreprise.java
@@ -1,7 +1,16 @@
 package modele;
 
+import java.util.ArrayList;
+
 //Test
 public class Entreprise
 {
-
+	/**
+	 * Nom de l'entreprise
+	 */
+	private String nom;
+	/**
+	 * Liste des bases dans lesquelle l'entreprise est présente
+	 */
+	private ArrayList<Base> Bases;
 }
