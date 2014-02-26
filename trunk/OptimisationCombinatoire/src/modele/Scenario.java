ba41cae43c7da915f14981051d05a4d5d0bb7a7e
raphael.pillie@gmail.com <raphael.pillie@gmail.com@f0f721dd-ee35-590d-ed42-93f5ef06c5ec>
2014-02-26 14:07:53 +0000

    Implémentation de la class Scénario

--- trunk/OptimisationCombinatoire/src/modele/Scenario.java
+++ trunk/OptimisationCombinatoire/src/modele/Scenario.java
@@ -1,6 +1,58 @@
 package modele;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Scenario
 {
+	/**
+	 * Liste des bases
+	 */
+	private ArrayList<Base> bases;
+	/**
+	 * Liste des entreprises
+	 */
+	private ArrayList<Entreprise> entreprises;
 
+	public Scenario(ArrayList<Base> bases, ArrayList<Entreprise> entreprises)
+	{
+		this.bases = bases;
+		this.entreprises = entreprises;
+	}
+
+	public Scenario()
+	{
+		this.bases = new ArrayList<Base>();
+		this.entreprises = new ArrayList<Entreprise>();
+	}
+
+	public boolean addBase(Base base)
+	{
+		return this.bases.add(base);
+	}
+
+	public boolean addEntreprise(Entreprise entreprise)
+	{
+		return this.entreprises.add(entreprise);
+	}
+
+	public ArrayList<Base> getBases()
+	{
+		return bases;
+	}
+
+	public void setBases(ArrayList<Base> bases)
+	{
+		this.bases = bases;
+	}
+
+	public ArrayList<Entreprise> getEntreprises()
+	{
+		return entreprises;
+	}
+
+	public void setEntreprises(ArrayList<Entreprise> entreprises)
+	{
+		this.entreprises = entreprises;
+	}
 }
