d1ad0d72883d7067f7d97a420da28196ae7ba8c5
raphael.pillie@gmail.com <raphael.pillie@gmail.com@f0f721dd-ee35-590d-ed42-93f5ef06c5ec>
2014-02-26 13:52:36 +0000

    Implementation de la class base

--- OptimisationCombinatoire/src/modele/Base.java
+++ OptimisationCombinatoire/src/modele/Base.java
@@ -1,6 +1,41 @@
 package modele;
 
+import java.util.ArrayList;
+
 public class Base
 {
+	/**
+	 * Nom de la base
+	 */
+	private String nom;
+	/**
+	 * Liste des entreprises présent dans la base
+	 */
+	private ArrayList<Entreprise> entreprises;
 
+	public Base(String nom, ArrayList<Entreprise> entreprises)
+	{
+		this.nom = nom;
+		this.entreprises = entreprises;
+	}
+
+	public String getNom()
+	{
+		return nom;
+	}
+
+	public void setNom(String nom)
+	{
+		this.nom = nom;
+	}
+
+	public ArrayList<Entreprise> getEntreprises()
+	{
+		return entreprises;
+	}
+
+	public void setEntreprises(ArrayList<Entreprise> entreprises)
+	{
+		this.entreprises = entreprises;
+	}
 }
