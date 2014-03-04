6c30c0893386fd55089eeb3b72a3abf31ce4a76a
yassine.badih@gmail.com <yassine.badih@gmail.com@f0f721dd-ee35-590d-ed42-93f5ef06c5ec>
2014-03-04 17:11:40 +0000

    Mise en place d'un algo glouton en prenant en compte comme critére de choix les bases ayant le plus grand nombre d'entreprise. Algo fonctionnel

--- OptimisationCombinatoire/src/modele/Scenario.java
+++ OptimisationCombinatoire/src/modele/Scenario.java
@@ -4,32 +4,77 @@ import java.util.TreeSet;
 
 public class Scenario
 {
-	/**
-	 * Liste des bases
-	 */
+	// Liste des bases
 	public static TreeSet<Base> bases;
-	/**
-	 * Liste des entreprises
-	 */
+	// Liste des entreprises
 	public static TreeSet<Entreprise> entreprises;
-
-	public Scenario(TreeSet<Base> bases, TreeSet<Entreprise> entreprises)
-	{
-		Scenario.bases = bases;
-		Scenario.entreprises = entreprises;
-	}
 
 	public Scenario()
 	{
 		Scenario.bases = new TreeSet<Base>();
 		Scenario.entreprises = new TreeSet<Entreprise>();
 	}
-
-	public static boolean addBase(Base base)
+	
+	/*
+	public Scenario(TreeSet<Base> bases, TreeSet<Entreprise> entreprises)
 	{
-		return Scenario.bases.add(base);
-	}
+		Scenario.bases = bases;
+		Scenario.entreprises = entreprises;
+	}*/
+	
+	public void loadDatas(String listeBase, String listeEntreprise)
+	{
+		// On initialise les parseurs
+		ParserBase parserBase = new ParserBase();
+		ParserScenarioBase parserScenarioBase = new ParserScenarioBase();
+		ParserScenarioEntreprise parserScenarioEntreprise = new ParserScenarioEntreprise();
+		// On recupère les listes que l'utilisateur fournit
+		parserScenarioBase.loadFile(listeBase);
+		parserScenarioEntreprise.loadFile(listeEntreprise);
 
+		// On charge les bases
+		for (Base base : bases)
+		{
+			parserBase.loadFile("Data/Bases/" + base.getNom());
+		}
+		
+		// On trie les bases en fonction des de leurs nombre d'entreprise
+		// il suffit de recréer un TreeSet avec les bases complete, et le
+		// comparator fait le reste
+		TreeSet<Base> basesTrie = new TreeSet<Base>();
+		for (Base base : bases)
+		{
+			basesTrie.add(base);
+		}
+		bases = basesTrie;
+		
+		// Meme opération pour les entreprise
+		Entreprise.triage = true;
+		TreeSet<Entreprise> entrepriseTrie = new TreeSet<Entreprise>();
+		for (Entreprise entreprise : entreprises)
+		{
+			entrepriseTrie.add(entreprise);
+		}
+		entreprises = entrepriseTrie;
+		Entreprise.triage = false;
+		
+		/*
+		 * System.out.println("Nombre de bases connu : " + bases.size());
+		 * System.out.println("Nombre d'entreprise recherché : " +
+		 * entreprises.size()); for (Entreprise entreprise : entreprises) {
+		 * System.out.println(entreprise.getNom()); for (Base base :
+		 * entreprise.getBases()) { System.out.println("\t" + base.getNom()); }
+		 * } System.out.println("Test de recherche sur la premiere base : " +
+		 * bases.first().getEntreprises().size()); for (Base base : bases) {
+		 * System.out.println(base.getNom()); for (Entreprise entreprise :
+		 * base.getEntreprises()) { System.out.println("\t" +
+		 * entreprise.getNom()); } }
+		 * System.out.println("Test de recherche sur la premiere entreprise : "
+		 * + entreprises.first().getNom() + " " +
+		 * entreprises.first().getBases().size());
+		 */
+	}
+	
 	public static void updateBase(Base baseUpdate)
 	{
 		for (Base base : bases)
@@ -53,12 +98,7 @@ public class Scenario
 			}
 		}
 	}
-
-	public static boolean addEntreprise(Entreprise entreprise)
-	{
-		return Scenario.entreprises.add(entreprise);
-	}
-
+	
 	public void launchGlouton()
 	{
 		AlgoRecherche algo = new AlgoGloutonMax();
@@ -71,55 +111,14 @@ public class Scenario
 		algo.rechercher(this);
 	}
 
-	public void loadDatas(String listeBase, String listeEntreprise)
+	public static boolean addBase(Base base)
 	{
-		// On initialise les parseurs
-		ParserBase parserBase = new ParserBase();
-		ParserScenarioBase parserScenarioBase = new ParserScenarioBase();
-		ParserScenarioEntreprise parserScenarioEntreprise = new ParserScenarioEntreprise();
+		return Scenario.bases.add(base);
+	}
 
-		// On recupère les listes que l'utilisateur fournit
-		parserScenarioBase.loadFile(listeBase);
-		parserScenarioEntreprise.loadFile(listeEntreprise);
-
-		// On chage les bases
-		for (Base base : bases)
-		{
-			parserBase.loadFile("Data/Bases/" + base.getNom());
-		}
-		// On trie les bases en fonction des de leurs nombre d'entreprise
-		// il suffit de recréer un TreeSet avec les bases complete, et le
-		// comparator fait le reste
-		TreeSet<Base> basesTrie = new TreeSet<Base>();
-		for (Base base : bases)
-		{
-			basesTrie.add(base);
-		}
-		bases = basesTrie;
-		// Meme opération pour les entreprise
-		Entreprise.triage = true;
-		TreeSet<Entreprise> entrepriseTrie = new TreeSet<Entreprise>();
-		for (Entreprise entreprise : entreprises)
-		{
-			entrepriseTrie.add(entreprise);
-		}
-		entreprises = entrepriseTrie;
-		Entreprise.triage = false;
-		/*
-		 * System.out.println("Nombre de bases connu : " + bases.size());
-		 * System.out.println("Nombre d'entreprise recherché : " +
-		 * entreprises.size()); for (Entreprise entreprise : entreprises) {
-		 * System.out.println(entreprise.getNom()); for (Base base :
-		 * entreprise.getBases()) { System.out.println("\t" + base.getNom()); }
-		 * } System.out.println("Test de recherche sur la premiere base : " +
-		 * bases.first().getEntreprises().size()); for (Base base : bases) {
-		 * System.out.println(base.getNom()); for (Entreprise entreprise :
-		 * base.getEntreprises()) { System.out.println("\t" +
-		 * entreprise.getNom()); } }
-		 * System.out.println("Test de recherche sur la premiere entreprise : "
-		 * + entreprises.first().getNom() + " " +
-		 * entreprises.first().getBases().size());
-		 */
+	public static boolean addEntreprise(Entreprise entreprise)
+	{
+		return Scenario.entreprises.add(entreprise);
 	}
 
 	public TreeSet<Base> getBases()
