091dfeb726238f0e13c51d1865bb90190fcda2d6
raphael.pillie@gmail.com <raphael.pillie@gmail.com@f0f721dd-ee35-590d-ed42-93f5ef06c5ec>
2014-03-30 18:24:03 +0000

    Correction de gloutonEntrepriseMax

--- trunk/OptimisationCombinatoire/src/modele/AlgoGloutonEntrepriseMax.java
+++ trunk/OptimisationCombinatoire/src/modele/AlgoGloutonEntrepriseMax.java
@@ -2,6 +2,8 @@ package modele;
 
 import java.util.ArrayList;
 
+import affichage.Affichage;
+
 public class AlgoGloutonEntrepriseMax extends AlgoRecherche
 {
 	private ArrayList<Base> bases;
@@ -13,27 +15,6 @@ public class AlgoGloutonEntrepriseMax extends AlgoRecherche
 	@Override
 	public void rechercher(Scenario scenario)
 	{
-		// On fait une copie des bases et on ne récupére que les bases ayant une
-		// liste d'entreprise différente de null,
-		// étant donné que nous avons supprimer toutes les entreprises inutile à
-		// notre scénario
-		this.bases = new ArrayList<Base>();
-		for (Base base : scenario.getBases())
-		{
-			if (base.getEntreprises().size() != 0)
-			{
-				this.bases.add(base);
-				System.out.println("la " + base.getNom() + " a pour taille: " + base.getEntreprises().size() + " est pour cout: " + base.getCout());
-			}
-		}
-
-		System.out.println("*****************************************");
-		this.sortCout(bases, basesTemp1);
-		for (Base base : basesTemp1)
-		{
-			System.out.println("la " + base.getNom() + " a pour taille: " + base.getEntreprises().size() + " est pour cout: " + base.getCout());
-		}
-
 		// On fait une copie des entreprises
 		this.entreprises = new ArrayList<Entreprise>();
 		for (Entreprise entreprise : scenario.getEntreprises())
@@ -41,83 +22,110 @@ public class AlgoGloutonEntrepriseMax extends AlgoRecherche
 			this.entreprises.add(entreprise);
 		}
 
+		this.bases = new ArrayList<Base>();
+		for (Base base : scenario.getBases())
+		{
+			base.getEntreprises().retainAll(entreprises);
+			if (base.getEntreprises().size() != 0)
+			{
+				this.bases.add(base);
+			}
+		}
+
+		this.sortCout(bases, basesTemp1);
+		StringBuilder text = new StringBuilder();
+		for (Base base : basesTemp1)
+		{
+			text.append("La " + base.getNom() + " a pour taille: " + base.getEntreprises().size() + " et pour cout: " + base.getCout() + "\n");
+			// Affichage.afficher("La " + base.getNom() + " a pour taille: " +
+			// base.getEntreprises().size() + " et pour cout: " +
+			// base.getCout());
+		}
+		Affichage.afficher(text);
 		this.coutOptimal = 0;
 		this.baseOptimal = new ArrayList<Base>();
-		this.recursive(entreprises, bases);
-		System.out.println(this.coutOptimal);
+		this.recursive(entreprises, basesTemp1);
+		Affichage.afficher(this.coutOptimal);
 		for (Base base : this.baseOptimal)
 		{
-			System.out.println(base.getNom());
+			Affichage.afficher(base.getNom());
 		}
 	}
 
 	private void recursive(ArrayList<Entreprise> entreprisesBase, ArrayList<Base> basesMax)
 	{
-		boolean checkEntreprises = false;
-
-		// On copie la liste des entreprise et des bases restante
-		ArrayList<Entreprise> entreprises = new ArrayList<Entreprise>();
-		ArrayList<Base> bases = new ArrayList<Base>();
-		for (Entreprise entrepriseTemp : entreprisesBase)
+		if (basesMax.size() != 0)
 		{
-			entreprises.add(entrepriseTemp);
-		}
-		for (Base base : basesMax)
-		{
-			bases.add(base);
-		}
+			boolean checkEntreprises = false;
 
-		// On prend la première base qui vient dans la liste
-		Base base = bases.get(0);
-		// Puis on la supprime de la liste pour plus tard
-		bases.remove(0);
-
-		// On parcour la liste des entreprises
-		ArrayList<Entreprise> entrepriseToRemove = new ArrayList<Entreprise>();
-		for (Entreprise entreprise : entreprises)
-		{
-			if (base.getEntreprises().contains(entreprise))
+			// On copie la liste des entreprise et des bases restante
+			ArrayList<Entreprise> entreprises = new ArrayList<Entreprise>();
+			ArrayList<Base> bases = new ArrayList<Base>();
+			for (Entreprise entrepriseTemp : entreprisesBase)
 			{
-				entrepriseToRemove.add(entreprise);
-				checkEntreprises = true;
+				entreprises.add(entrepriseTemp);
 			}
-		}
-		// On supprime les entreprises qui sont dans la base actuel
-		for (Entreprise entrepriseRemoveTemp : entrepriseToRemove)
-		{
-			entreprises.remove(entrepriseRemoveTemp);
-		}
+			for (Base base : basesMax)
+			{
+				bases.add(base);
+			}
 
-		// Si on a découvert des entreprises dans la base actuel, on additionne
-		// le cout de la base actuel au cout optimale
-		// Et on rajoute la base actuel dans la la liste des bases optimales
-		if (checkEntreprises)
-		{
-			coutOptimal = coutOptimal + base.getCout();
-			baseOptimal.add(base);
-		}
+			// On prend la première base qui vient dans la liste
+			Base base = bases.get(0);
+			// Puis on la supprime de la liste pour plus tard
+			bases.remove(0);
 
-		if ((entreprises.size() != 0) && (bases.size() != 0))
-		{
-			recursive(entreprises, bases);
+			// On parcour la liste des entreprises
+			ArrayList<Entreprise> entrepriseToRemove = new ArrayList<Entreprise>();
+			for (Entreprise entreprise : entreprises)
+			{
+				if (base.getEntreprises().contains(entreprise))
+				{
+					entrepriseToRemove.add(entreprise);
+					checkEntreprises = true;
+				}
+			}
+			// On supprime les entreprises qui sont dans la base actuel
+			for (Entreprise entrepriseRemoveTemp : entrepriseToRemove)
+			{
+				entreprises.remove(entrepriseRemoveTemp);
+			}
+
+			// Si on a découvert des entreprises dans la base actuel, on
+			// additionne
+			// le cout de la base actuel au cout optimale
+			// Et on rajoute la base actuel dans la la liste des bases optimales
+			if (checkEntreprises)
+			{
+				coutOptimal = coutOptimal + base.getCout();
+				baseOptimal.add(base);
+			}
+
+			if ((entreprises.size() != 0) && (bases.size() != 0))
+			{
+				recursive(entreprises, bases);
+			}
 		}
 	}
 
 	void sortCout(ArrayList<Base> bases, ArrayList<Base> basesActuel)
 	{
-		Base baseCoutMax = bases.get(0);
-		for (Base base : bases)
+		if (bases.size() > 0)
 		{
-			if (base.getCout() > baseCoutMax.getCout())
+			Base baseCoutMax = bases.get(0);
+			for (Base base : bases)
 			{
-				baseCoutMax = base;
+				if (base.getEntreprises().size() > baseCoutMax.getEntreprises().size())
+				{
+					baseCoutMax = base;
+				}
 			}
-		}
-		basesActuel.add(baseCoutMax);
-		bases.remove(baseCoutMax);
-		if (bases.size() != 0)
-		{
-			sortCout(bases, basesActuel);
+			basesActuel.add(baseCoutMax);
+			bases.remove(baseCoutMax);
+			if (bases.size() != 0)
+			{
+				sortCout(bases, basesActuel);
+			}
 		}
 	}
 
